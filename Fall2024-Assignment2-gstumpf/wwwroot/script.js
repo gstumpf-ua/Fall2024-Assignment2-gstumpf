0502b5308b728f1ddb071c9c9a387624f20a95dd
Gavin Stumpf <gstumpf@crimson.ua.edu>
2024-10-07 21:44:03 -0500

    Extra Credit Code

--- Fall2024-Assignment2-gstumpf/wwwroot/script.js
+++ Fall2024-Assignment2-gstumpf/wwwroot/script.js
@@ -27,8 +27,36 @@ function apiSearch() {
             alert('error');
         });
 }
-function showTime() {
 
+function luckySearch() {
+    var params = {
+        'q': $('#query').val(),
+        'count': 1, 
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '2af320e8e947424ea23a345116925531'
+        }
+    })
+        .done(function (data) {
+            if (data.webPages && data.webPages.value.length > 0) {
+                // Redirect to the first result's URL
+                window.location.href = data.webPages.value[0].url;
+            } else {
+                alert('No results found.');
+            }
+        })
+        .fail(function () {
+            alert('error');
+        });
+}
+
+function showTime() {
     var now = new Date();
     var hours = now.getHours().toString().padStart(2, '0');
     var minutes = now.getMinutes().toString().padStart(2, '0');
@@ -36,35 +64,48 @@ function showTime() {
     console.log(timeString);
 
     $('#time').html('<p>' + timeString + '</p>');
-
-
     $('#time').css('visibility', 'visible').dialog({
         modal: true,
         title: 'Current Time',
         width: 300,
-        show: { effect: 'fade', duration: 800 },  
-        hide: { effect: 'fade', duration: 800 }   
+        show: { effect: 'fade', duration: 800 },
+        hide: { effect: 'fade', duration: 800 }
     });
 }
-function changeBackground() {
-    var currentBackground = $('body').css('background-image');
-    var image1 = 'url(Background1.jpg)';  
-    var image2 = 'url(Background2.jpg)';  
 
-    if (currentBackground.includes('Background1.jpg')) {
-        $('body').css('background-image', image2);
+var currentBackgroundIndex = 0;
+var lightSet = ['Background2.jpg', 'Background3.jpg'];  
+var darkSet = ['Background1.jpg', 'Background4.jpg']; 
+var isAlternate = false;  
+var imageCycleInterval;  
+
+function cycleBackground(images) {
+    currentBackgroundIndex = (currentBackgroundIndex + 1) % images.length;
+    $('body').css('background-image', 'url(' + images[currentBackgroundIndex] + ')');
+}
+
+function startImageCycle(images) {
+    clearInterval(imageCycleInterval);  
+    imageCycleInterval = setInterval(function () {
+        cycleBackground(images);
+    }, 10000);  
+}
+
+function changeImageSet() {
+    if (isAlternate) {
+        $('body').css('background-image', 'url(' + lightSet[currentBackgroundIndex] + ')');
+        startImageCycle(lightSet);  
     } else {
-        $('body').css('background-image', image1);
+        $('body').css('background-image', 'url(' + darkSet[currentBackgroundIndex] + ')');
+        startImageCycle(darkSet);  
     }
+    isAlternate = !isAlternate;  
 }
 
 $(document).ready(function () {
-    
+    startImageCycle(lightSet);
     $('#searchBtn').click(apiSearch);
-
-    
+    $('#luckyBtn').click(luckySearch);
     $('#timeBtn').click(showTime);
-
-    
-    $('#siteName').click(changeBackground);
+    $('#siteName').click(changeImageSet);
 });
